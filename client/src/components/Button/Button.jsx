82e0741acea532cf995338d84b739949f885230d
Pepe <programacion2@luaediciones.com>
2021-01-16 21:51:51 +0100

    Se añaden los estilos para el componente Button

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,20 +1,63 @@
+import PropTypes from 'prop-types';
+import './Button.scss';
+
 const Button = ({
   type = 'button',
-  children,
   extraClasses = '',
   id = '',
-  onClick
+  isPrimary = false,
+  isSmall = false,
+  isBig = false,
+  onClick,
+  children
 }) => {
+  let styleProps = '';
+  styleProps += isPrimary ? 'is-primary ' : '';
+  styleProps += isSmall ? 'is-small ' : '';
+  styleProps += isBig ? 'is-big ' : '';
+  
   return (
     <button
       type={type}
-      className={`button ${extraClasses}`.trimRight()}
+      className={`button ${styleProps} ${extraClasses}`.trimRight()}
       onClick={onClick || null}
       id={id !== '' ? id : null}
     >
       {children}
     </button>
   );
+}
+
+Button.propTypes = {
+  /**
+   * Button type.
+   */
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+
+  /**
+   * Additional CSS classes.
+   */
+  extraClasses: PropTypes.string,
+
+  /**
+   * Unique DOM identifier.
+   */
+  id: PropTypes.string,
+
+  /**
+   * Indicates if is a primary button.
+   */
+  isPrimary: PropTypes.bool,
+
+  /**
+   * Function to execute when the button is clicked.
+   */
+  onClick: PropTypes.func,
+  
+  /**
+   * React children nodes.
+   */
+  children: PropTypes.node
 }
 
 export default Button;
