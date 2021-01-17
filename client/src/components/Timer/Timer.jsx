c020d8f9e122ead88b3a8aabe4a2a79dead11009
Pepe <programacion2@luaediciones.com>
2021-01-18 00:57:42 +0100

    Se pasa la funcionalidad del contador a un hook reutilizable

--- client/src/components/Timer/Timer.jsx
+++ client/src/components/Timer/Timer.jsx
@@ -1,42 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useTimer } from 'hooks/useTimer';
 import { FlexContainer } from 'components';
 import './Timer.scss';
 
 const Timer = ({ initialTime }) => {
-  const [ seconds, setSeconds ] = useState(0);
-  const [ minutes, setMinutes ] = useState(0);
-  const [ hours, setHours ] = useState(0);
-  const [ days, setDays ] = useState(0);
-
-  const MS_TO_SECONDS = 1000;
-  const MS_TO_MINUTES = 60000;
-  const MS_TO_HOURS = 3600000;
-  const MS_TO_DAYS = 86400000;
-
-  const updateTimer = (initialTime) => {
-    const now = Date.now();
-    const lastLogin = new Date(initialTime).getTime();
-    const difference = now - lastLogin;
-    const secondsNow = Math.floor(difference / MS_TO_SECONDS % 60);
-    const minutesNow = Math.floor(difference / MS_TO_MINUTES % 60);
-    const hoursNow = Math.floor(difference / MS_TO_HOURS % 24);
-    const daysNow = Math.floor(difference / MS_TO_DAYS);
-    
-    setSeconds(secondsNow);
-    setMinutes(minutesNow);
-    setHours(hoursNow);
-    setDays(daysNow);
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      updateTimer(initialTime);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
+  const { days, hours, minutes, seconds } = useTimer(initialTime);
   
   return (
     <div className="timer">
