fc2643e388bd3ea487a9219536530d8ab2ed13a5
Pepe <programacion2@luaediciones.com>
2021-01-18 10:35:30 +0100

    Se añade documentación para Storybook de algunos componentes

--- client/src/components/Field/Field.jsx
+++ client/src/components/Field/Field.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import './Field.scss';
 
 const Field = ({
@@ -15,6 +16,38 @@ const Field = ({
       )}
     </div>
   );
+}
+
+Field.propTypes = {
+  /**
+   * Extra CSS class names.
+   */
+  extraClasses: PropTypes.string,
+
+  /**
+   * React node of type Input.
+   */
+  Input: PropTypes.node,
+
+  /**
+   * React node of type Label.
+   */
+  Label: PropTypes.node,
+
+  /**
+   * If defined, it's used to display a warning or error message.
+   */
+  message: PropTypes.shape({
+    /**
+     * Type of message.
+     */
+    type: PropTypes.oneOf(['error', 'warning']),
+    
+    /**
+     * Message to be displayed.
+     */
+    message: PropTypes.string
+  })
 }
 
 export default Field;
